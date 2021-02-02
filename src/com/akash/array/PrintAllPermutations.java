71db21a862ac4ac4bec2fe0a87447225cad3869a
Akash Sharma <akash6.sharma@paytm.com>
2021-02-02 10:30:22 +0530

    array notes

--- src/com/akash/array/PrintAllPermutations.java
+++ src/com/akash/array/PrintAllPermutations.java
@@ -1,37 +1,67 @@
 package com.akash.array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// https://leetcode.com/problems/permutations/discuss/18239/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partioning)
 public class PrintAllPermutations {
 
 	public static void main(String args[]) {
-		String str = "ABCD";
-		printAllPermutations(str.toCharArray(), 0, str.length() - 1);
-	}
+		int arr[] = {1,2,3,4};
+		List<List<Integer>> result = permute(arr);
+		for(List<Integer> temp : result) {
+			System.out.println(Arrays.asList(temp));
+		}
 
-	static void printAllPermutations(char arr[], int start, int end) {
-		if (start > end) {
-			return;
-		}
-		if (start == end) {
-			print(arr);
-			return;
-		}
-		for (int i = start; i <= end; i++) {
-			swap(arr, start, i);
-			printAllPermutations(arr, start + 1, end);
-			swap(arr, start, i);
+		System.out.println("<==========>");
+
+		int duplicateArr[] = {1,2,1};
+		List<List<Integer>> uniqueResult = permuteUnique(duplicateArr);
+		for(List<Integer> temp : uniqueResult) {
+			System.out.println(Arrays.asList(temp));
 		}
 	}
 
-	static void print(char arr[]) {
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i]);
-		}
-		System.out.println();
+	public static List<List<Integer>> permute(int[] nums) {
+		List<List<Integer>> list = new ArrayList<>();
+		// Arrays.sort(nums); // not necessary
+		backtrack(list, new ArrayList<>(), nums);
+		return list;
 	}
 
-	static void swap(char arr[], int pos1, int pos2) {
-		char temp = arr[pos1];
-		arr[pos1] = arr[pos2];
-		arr[pos2] = temp;
+	private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+		if(tempList.size() == nums.length){
+			list.add(new ArrayList<>(tempList));
+		} else{
+			for(int i = 0; i < nums.length; i++){
+				if(tempList.contains(nums[i])) continue; // element already exists, skip
+				tempList.add(nums[i]);
+				backtrack(list, tempList, nums);
+				tempList.remove(tempList.size() - 1);
+			}
+		}
+	}
+
+	public static List<List<Integer>> permuteUnique(int[] nums) {
+		List<List<Integer>> list = new ArrayList<>();
+		Arrays.sort(nums);
+		backtrack(list, new ArrayList<>(), nums, new boolean[nums.length]);
+		return list;
+	}
+
+	private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, boolean [] used){
+		if(tempList.size() == nums.length){
+			list.add(new ArrayList<>(tempList));
+		} else{
+			for(int i = 0; i < nums.length; i++){
+				if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i - 1]) continue;
+				used[i] = true;
+				tempList.add(nums[i]);
+				backtrack(list, tempList, nums, used);
+				used[i] = false;
+				tempList.remove(tempList.size() - 1);
+			}
+		}
 	}
 }
