3fe49331aeacbb53ccb77fe5f169b6eace256627
Akash Sharma <akash6.sharma@paytm.com>
2021-02-04 15:14:52 +0530

    array notes

--- src/com/akash/array/PrintAllPermutations.java
+++ src/com/akash/array/PrintAllPermutations.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class PrintAllPermutations {
 
 	public static void main(String args[]) {
-		int arr[] = {1,2,3,4};
+		/*int arr[] = {1,2,3,4};
 		List<List<Integer>> result = permute(arr);
 		for(List<Integer> temp : result) {
 			System.out.println(Arrays.asList(temp));
@@ -19,6 +19,11 @@ public class PrintAllPermutations {
 		int duplicateArr[] = {1,2,1};
 		List<List<Integer>> uniqueResult = permuteUnique(duplicateArr);
 		for(List<Integer> temp : uniqueResult) {
+			System.out.println(Arrays.asList(temp));
+		}*/
+		int arr[] = {1,2,3,4};
+		List<List<Integer>> result = subsets(arr);
+		for(List<Integer> temp : result) {
 			System.out.println(Arrays.asList(temp));
 		}
 	}
@@ -62,6 +67,39 @@ public class PrintAllPermutations {
 				used[i] = false;
 				tempList.remove(tempList.size() - 1);
 			}
+		}
+	}
+
+	public static List<List<Integer>> subsets(int[] nums) {
+		List<List<Integer>> list = new ArrayList<>();
+		Arrays.sort(nums);
+		backtrackSubsets(list, new ArrayList<>(), nums, 0);
+		return list;
+	}
+
+	private static void backtrackSubsets(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+		list.add(new ArrayList<>(tempList));
+		for(int i = start; i < nums.length; i++){
+			tempList.add(nums[i]);
+			backtrackSubsets(list, tempList, nums, i + 1);
+			tempList.remove(tempList.size() - 1);
+		}
+	}
+
+	public static List<List<Integer>> subsetsWithDup(int[] nums) {
+		List<List<Integer>> list = new ArrayList<>();
+		Arrays.sort(nums);
+		backtrackSubsetsWithDup(list, new ArrayList<>(), nums, 0);
+		return list;
+	}
+
+	private static void backtrackSubsetsWithDup(List<List<Integer>> list, List<Integer> tempList, int [] nums, int start){
+		list.add(new ArrayList<>(tempList));
+		for(int i = start; i < nums.length; i++){
+			if(i > start && nums[i] == nums[i-1]) continue; // skip duplicates
+			tempList.add(nums[i]);
+			backtrackSubsetsWithDup(list, tempList, nums, i + 1);
+			tempList.remove(tempList.size() - 1);
 		}
 	}
 }
